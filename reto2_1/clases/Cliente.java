642674b52163829f80c081587579363ea5a45e3b
Pili-73 <DAM1@aula11-pc15>
2025-01-20 12:43:53 +0100

    oo

--- reto2_1/clases/Cliente.java
+++ reto2_1/clases/Cliente.java
@@ -1,5 +1,21 @@
 package clases;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cliente {
+	
+	private int id;
+	private String nombre;
+	private Map<Integer, List<Producto>> compras = new HashMap<Integer, List<Producto>>();
+
+	
 
 }
+
+
+/*Y por último tendremos a los clientes que compran los productos de las tiendas, de los que tenemos el nombre
+y lista de compras. La lista de compras será un HashMap donde la clave será la tienda y el valor una lista de
+productos que se compran en esa tienda. Tendrá un método de comprar producto, que recibirá la tienda y el
+producto a comprar. Y otro método de gasto total que devolverá el importe de todas las compras que haya
+realizado.*/
