ad93c2fe99dc27f370f96b577d5882652bef798b
Pili-73 <DAM1@aula11-pc15>
2025-01-20 12:59:38 +0100

    clase cliente

--- reto2_1/src/clases/Cliente.java
+++ reto2_1/src/clases/Cliente.java
@@ -1,16 +1,34 @@
 package clases;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class Cliente {
 	
 	private int id;
 	private String nombre;
-	private Map<Tienda, List<Producto>> compras = new HashMap<Tienda, List<Producto>>();
+	private Map<Tienda, List<Producto>> compras;
+	
+	
+	public Cliente() {
+		this.compras = new HashMap<Tienda, List<Producto>>();
+		
+	}
+
+	public Cliente(Map<Tienda, List<Producto>> compras, int id, String nombre) {
+		super();
+		this.compras = compras;
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -27,6 +45,27 @@ public class Cliente {
 		this.compras = compras;
 	}
 
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+	public void comprarProducto(Tienda tienda, Producto producto) {
+		 compras.put(tienda, producto);
+	}
+	
+	public void gastoTotal() {
+		Tienda tienda;
+		Producto valor;
+		double suma = 0;
+		Iterator<Integer> claves = compras.keySet().iterator();
+		while (claves.hasNext()) {
+			tienda = claves.next();
+			valor = compras.get(tienda);
+			suma += valor.precio;
+
+		}
+	}
 	
 
 }
